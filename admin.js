08328c2230fedde20a77ee74d6b0bbf0224ce500
Aytern <104952978+Aytern@users.noreply.github.com>
2025-09-05 11:54:34 +0800

    Delete admin.js

--- admin.js
+++
@@ -1,8 +0,0 @@
-import api from './index'
-
-export const adminAPI = {
-  getUsers: (params) => api.get('/api/admin/users', { params }),
-  getUser: (id) => api.get(`/api/admin/users/${id}`),
-  deleteUser: (id) => api.delete(`/api/admin/users/${id}`),
-  updateUser: (id, data) => api.put(`/api/admin/users/${id}`, data)
-}
